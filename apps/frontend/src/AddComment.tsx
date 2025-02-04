9c997c1b2a5df5d13bc585b051337252b3e2fa0b
Antti Ellilä <antti@antti.codes>
2025-02-04 13:13:46 +0200

    feat: resize comment box when wrapping lines

--- apps/frontend/src/AddComment.tsx
+++ apps/frontend/src/AddComment.tsx
@@ -1,12 +1,46 @@
 import { useStickyState } from "@/useStickyState.js";
 import { useQueryClient } from "@tanstack/react-query";
 import { CircleX, SendHorizontal } from "lucide-react";
-import { useCallback, useState, type FormEvent, type KeyboardEvent } from "react";
+import { useCallback, useRef, useState, type FormEvent, type KeyboardEvent } from "react";
+
+let shadow: HTMLTextAreaElement | null = null;
+function getLineCount(textarea: HTMLTextAreaElement) {
+  if (shadow == null) {
+    shadow = document.createElement("textarea");
+    // not visible
+    shadow.style.height = "0";
+    shadow.style.overflow = "hidden";
+    // not messing up calculations
+    shadow.style.border = "none";
+    shadow.style.padding = "0";
+    // just position it somewhere out of the way
+    shadow.style.position = "absolute";
+    shadow.style.left = "0";
+    shadow.style.top = "0";
+    shadow.style.zIndex = "-1";
+    document.body.appendChild(shadow);
+  }
+  // get necessary properties for calculations
+  const style = window.getComputedStyle(textarea);
+  const paddingLeft = parseInt(style.paddingLeft);
+  const paddingRight = parseInt(style.paddingRight);
+  let lineHeight = parseInt(style.lineHeight);
+  if (isNaN(lineHeight)) lineHeight = parseInt(style.fontSize);
+
+  // mimick the real textarea 
+  shadow.style.width = textarea.clientWidth - paddingLeft - paddingRight + "px";
+  shadow.style.font = style.font;
+  shadow.style.letterSpacing = style.letterSpacing;
+  shadow.value = textarea.value;
+
+  return Math.max(1, Math.floor(shadow.scrollHeight / lineHeight));
+}
 
 export function AddComment({ post }: { post: string }) {
   const queryClient = useQueryClient();
   const [name, setName] = useStickyState("name", "");
   const [content, setContent] = useState("");
+  const contentRef = useRef<HTMLTextAreaElement | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   const handleSubmit = useCallback(
@@ -58,7 +92,7 @@ export function AddComment({ post }: { post: string }) {
           <div className="relative grow">
             <input
               id="name"
-              className="flex h-9 w-full rounded-md border-none bg-secondary py-1 pl-3 pr-[3.25rem] shadow-sm transition-colors focus:outline-none focus:ring-1 focus:ring-primary md:text-sm focus:[&:user-invalid]:ring-red-500"
+              className="md:text-sm flex h-9 w-full rounded-md border-none bg-secondary py-1 pl-3 pr-[3.25rem] shadow-sm transition-colors focus:outline-none focus:ring-1 focus:ring-primary focus:[&:user-invalid]:ring-red-500"
               value={name}
               pattern=".*\S.*"
               onInvalid={(e) => {
@@ -82,14 +116,15 @@ export function AddComment({ post }: { post: string }) {
         </div>
         <div className="flex w-full flex-row gap-x-2">
           <label className="w-16 self-center justify-self-end text-right" htmlFor="message">
-            <span className="xs:inline-block hidden">Message</span>
+            <span className="hidden xs:inline-block">Message</span>
             <span className="xs:hidden">Msg</span>
           </label>
           <div className="relative grow">
             <textarea
               id="message"
-              className="flex max-h-40 min-h-9 w-full resize-none rounded-md border-none bg-secondary py-1 pl-3 pr-[3.25rem] shadow-sm transition-colors focus:outline-none focus:ring-1 focus:ring-primary md:text-sm focus:[&:user-invalid]:ring-red-500"
+              className="md:text-sm flex max-h-40 min-h-9 w-full resize-none rounded-md border-none bg-secondary py-1 pl-3 pr-[3.25rem] shadow-sm transition-colors focus:outline-none focus:ring-1 focus:ring-primary focus:[&:user-invalid]:ring-red-500"
               value={content}
+              ref={contentRef}
               onChange={(e) => {
                 (e.target as HTMLTextAreaElement).setCustomValidity("");
                 setContent(e.target.value);
@@ -98,7 +133,7 @@ export function AddComment({ post }: { post: string }) {
               }}
               minLength={1}
               maxLength={512}
-              rows={Math.max(1, content.split("\n").length)}
+              rows={contentRef.current ? getLineCount(contentRef.current) : 1}
               autoComplete="off"
               required
             />
